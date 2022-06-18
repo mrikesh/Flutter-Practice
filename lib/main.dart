bf2f9df250e33c2691543a79e82071a59e67ad89
mrikesh <mrikesh648@gmail.com>
2022-06-18 10:03:12 +0545

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,335 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "MyApp",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: home(),
+    );
+  }
+}
+
+class home extends StatefulWidget {
+  const home({Key? key}) : super(key: key);
+
+  @override
+  State<home> createState() => _homeState();
+}
+
+class _homeState extends State<home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text(
+          "Application",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Card(
+            child: Column(
+              children: [
+                Image.network(
+                  "https://media.gettyimages.com/vectors/welcome-banner-speech-bubble-label-ribbon-template-vector-stock-vector-id1292561966?s=612x612",
+                  fit: BoxFit.contain,
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        hintText: "Email", border: OutlineInputBorder()),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        hintText: "Password", border: OutlineInputBorder()),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Login()),
+                    );
+                  },
+                  child: Text("Login"),
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.all(16.0),
+                    minimumSize: Size(485, 50),
+                    backgroundColor: Colors.blue,
+                    primary: Colors.white,
+                    textStyle: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => CreateAccount()),
+                          );
+                        },
+                        child: Text("Create Account"),
+                        style: TextButton.styleFrom(
+                          padding: const EdgeInsets.all(16.0),
+                          textStyle: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ForgetPassword()),
+                          );
+                        },
+                        child: Text("Forget Password?"),
+                        style: TextButton.styleFrom(
+                          padding: const EdgeInsets.all(16.0),
+                          textStyle: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            // DrawerHeader(
+            //   child:Text(
+            //     "Hello",
+            //     style: TextStyle(color: Colors.white)),
+            //   decoration: BoxDecoration(color: Colors.indigo),
+            //    ),
+            UserAccountsDrawerHeader(
+              accountName: Text("User Name"),
+              accountEmail: Text("user123@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Account"),
+              subtitle: Text("Personal"),
+              trailing: Icon(Icons.edit),
+            ),
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text("Email"),
+              subtitle: Text("user@email.com"),
+              trailing: Icon(Icons.send),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.edit),
+      ),
+    );
+  }
+}
+
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello"),
+      ),
+    );
+  }
+}
+
+class CreateAccount extends StatelessWidget {
+  const CreateAccount({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Create New Account"),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(30),
+          child: Column(
+            children: [
+              Text(
+              "Register",
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.start,
+              ),
+            SizedBox(
+              height: 10,
+            ),  
+            Text(
+              "Easy and Quick",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w200,
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "First Name",
+                  border: OutlineInputBorder(),
+                  ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Last Name",
+                  border: OutlineInputBorder(),
+                  ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Email",
+                  border: OutlineInputBorder(),
+                  ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Password",
+                  border: OutlineInputBorder(),
+                  ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Re-type Password",
+                  border: OutlineInputBorder(),
+                  ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ForgetPassword extends StatelessWidget {
+  const ForgetPassword({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Create New Password"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          children: [
+            Text(
+              "Register",
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.start,
+              ),
+            SizedBox(
+              height: 10,
+            ),  
+            Text(
+              "Easy and Quick",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w200,
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Enter New Password",
+                border: OutlineInputBorder(),
+                ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Re-type New Password",
+                border: OutlineInputBorder(),
+                ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
